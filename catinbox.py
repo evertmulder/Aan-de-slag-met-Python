e19bdadc25055e218b1b41aa19337a2b3985b56a
Evert Mulder <evertmulder@gmail.com>
2018-10-16 23:31:50 +0200

    Now the boxes can be in a line and in a circle

--- catinbox.py
+++ catinbox.py
@@ -4,6 +4,7 @@ from random import randrange, choice
 class Cat:
     def __init__(self):
         self.box_index = None
+        self.can_jump_from_first_to_last = True
 
     def jump_in_box(self, boxes):
         if self.box_index:
@@ -15,11 +16,16 @@ class Cat:
     def move(self, boxes):
         boxes[self.box_index].remove_cat_from_box()
         new_index = self.box_index + choice([-1, 1])
-        if new_index < 0:
-            new_index = len(boxes) - 1
-        elif new_index > len(boxes) - 1:
-            new_index = 0
-
+        if self.can_jump_from_first_to_last:
+            if new_index < 0:
+                new_index = len(boxes) - 1
+            elif new_index > len(boxes) - 1:
+                new_index = 0
+        else:
+            if new_index < 0:
+                new_index = 1
+            elif new_index > len(boxes) - 1:
+                new_index = len(boxes) - 1
         boxes[new_index].put_cat_in_box()
         self.box_index = new_index
 
@@ -42,11 +48,12 @@ class Box:
 
 
 class Simulation:
-    def __init__(self, number_of_boxes):
+    def __init__(self, number_of_boxes, cat_can_jump_from_first_to_last):
         self._boxes = [Box(box_no) for box_no in range(number_of_boxes)]
         self._tries = 0
         self._cat_found = False
         self._cat = Cat()
+        self._cat.can_jump_from_first_to_last = cat_can_jump_from_first_to_last
         self._cat.jump_in_box(self._boxes)
 
     def check_random_box(self):
@@ -75,12 +82,14 @@ simulation_runs = 2000
 no_boxes = 100
 all_simulation_results = []
 
+cat_can_jump_from_first_to_last = True
+
 for i in range(simulation_runs):
-    s = Simulation(no_boxes)
+    s = Simulation(no_boxes, cat_can_jump_from_first_to_last)
     s.run_until_cat_found()
     tries = s.get_tries()
     all_simulation_results.append(tries)
-
+str()
 average_tries = sum(all_simulation_results) / simulation_runs
 print("Gemiddeld %i pogingen om de kat te vinden in %i dozen. Runs: %i. Min pogingen: %i. Max pogingen: %i" %
       (average_tries, no_boxes, simulation_runs, min(all_simulation_results), max(all_simulation_results)))
